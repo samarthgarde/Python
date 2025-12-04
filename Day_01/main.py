fb6ab467ee1b886d9c3eea687367464b5e3b284e
samarthgarde007 <samarthgarde007@gmail.com>
2025-12-04 14:04:52 +0530

    Day_01

--- Day_01/main.py
+++ Day_01/main.py
@@ -1,5 +1,25 @@
-print ("Hello, World!")
-
-
+print ("Hello World!")
 print ("Welcome to Day 01!")
+print ('Python is Awesome')
+
+
+# Comments
+
+# This is an comment
+
+# Multiline comments
+
+"""
+Hello Guys
+I am learning python from Skillected
+for Creating Automtion, Writing Scripts
+"""
+
+'''
+Hello this is multiline comments
+'''
+
+# Number printing
+print(9383)
+print(3+2)
 
