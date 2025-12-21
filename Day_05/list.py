6868a865a855a460b6fc048856a49f54dd4a8c03
samarthgarde007 <samarthgarde007@gmail.com>
2025-12-21 16:13:56 +0530

    Day_05 learn list and its methods

---
+++ Day_05/list.py
@@ -0,0 +1,67 @@
+fruits = ["apple", "banana", "mango"]
+print(fruits)
+
+# List indexing
+print(fruits[0])   # apple
+print(fruits[-1])  # mango
+
+# List slicing
+
+print(fruits[0:2])  # ['apple', 'banana']
+
+
+# List Methods
+
+numbers = [10, 20, 30]
+
+numbers.append(40)
+numbers.insert(1, 15)
+numbers.remove(20)
+numbers.pop()
+numbers.sort()
+numbers.reverse()
+
+print(numbers)
+
+# Loop with List (for loop basics)
+
+fruits = ["apple", "banana", "mango"]
+
+for fruit in fruits:
+    print(fruit)
+
+
+# User Input + List
+
+names = []
+
+names.append(input("Enter name 1: "))
+names.append(input("Enter name 2: "))
+
+print(names)
+
+
+names = ['samarth','omkaer','prabhu','ramesh']
+
+print (names[:5])   # All numbers are printed
+
+
+numbers = [10,33,22,221,2292,22]
+ 
+largest_number = max(numbers)  #  find_out largest number
+print (f"the largest number is: {largest_number}")
+
+
+numbers = []
+
+numbers.append(input("Enter name 1: "))
+numbers.append(input("Enter name 2: "))
+numbers.append(input("Enter name 3: "))
+numbers.append(input("Enter name 4: "))
+numbers.append(input("Enter name 5: "))
+
+print (numbers)
+
+
+
+
