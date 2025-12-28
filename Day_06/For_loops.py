d35f308c8290c30b99023a7b42b81bb57c6eca4e
samarthgarde007 <samarthgarde007@gmail.com>
2025-12-28 09:17:29 +0530

    Added Day_06 folder

---
+++ Day_06/For_loops.py
@@ -0,0 +1,47 @@
+# Looping Through a int
+
+#This is for the loop will print numbers from 1 to 100
+for i in range(1, 101):        # range() function
+    print(i)
+
+
+# Looping Through a List
+list_of_numbers = [1, 2, 3, 4, 5]
+for number in list_of_numbers:
+   print(number)
+
+# looping through a string
+for number in list_of_numbers:
+    print(number)
+
+for char in "Hello":
+   print(char)
+
+for character in "python":
+   print(character)
+
+# looping through a list
+names = ["samarth", "omkaer", "prabhu", "ramesh"]
+for name in names:
+    print(name)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+# looping through a dictionary
+my_dict = {'a':1,'b':2,'c':3}
+for key, value in my_dict.items():
+   print(f"key: {key}, value: {value}")
+
+# looping through a Sets
+my_set = {1,2,3,4,5}
+for key, value in my_set:
+   print(my_set)
+
+list1 = [10,20,30,40]
+
+for item in list1:
+   print(item)
+
+
---
+++ Day_06/While_loops.py
@@ -0,0 +1,32 @@
+i = 1
+while i < 6:
+   print(i)
+   i += 1
+
+
+# break statement
+
+i = 1
+while i < 6:
+   print(i)
+   if i == 3:
+       break
+   i += 1
+
+# Continue Statement
+
+i = 0
+while i < 6:
+   i += 1
+   if i == 3:
+       continue
+   print(i)
+
+# Else Statement
+
+i = 1
+while i < 6:
+   print(i)
+   i += 1
+else:
+   print("i is no longer less than 6")
