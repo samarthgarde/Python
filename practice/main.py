072c549901e725604cad07138db44f68d8d50b11
samarthgarde007 <samarthgarde007@gmail.com>
2026-01-02 20:43:26 +0530

    solved practice question

---
+++ practice/main.py
@@ -0,0 +1,39 @@
+# a = float(input("Enter the name:"))
+# b = float(input("Enter your last name"))
+
+# c = a + b
+
+# print(f"your first float is {a},and second float is {b},total is {c}")
+
+# # Division
+
+# num3 = float(input("Enter the dividend for division: "))
+# num4 = float(input("Enter the divisor of "))
+# if num4 == 0:
+#     print("Error: Division by zero is not allowed")
+# else:
+#     div_result =  num3 / num4
+#     print(f"Division: {num3} / {num4} = {div_result}")
+
+# Area of triangle
+
+# height = float(input("Enter height:"))
+# width = float(input("Enter height:"))
+
+# area = 0.5 * height * width
+
+# print(f"the area of triange is {area}")
+
+# Input two variables
+
+a = input("Enter the value of the first variable (a): ")
+b = input("Enter the value of the second variable (b): ")
+# Display the original values
+print(f"original value: a={a},b={b}")
+# Swap the values using a temporary variable
+temp = a
+a = b
+b = temp
+# Display the swapped values
+print(f"Swapped values: a = {a}, b = {b}")
+
